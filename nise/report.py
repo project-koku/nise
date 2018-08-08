08e4679d55d0851a1f8992d45153850fe4d95b93
Douglas Curtis <docurtis@redhat.com>
2018-08-08 10:13:39 -0400

    basically working

--- nise/report.py
+++ nise/report.py
@@ -15,11 +15,14 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """Module responsible for generating the cost and usage report."""
+import calendar
 import csv
 import gzip
 import os
+from datetime import datetime
 from tempfile import NamedTemporaryFile
 
+from dateutil.relativedelta import relativedelta
 from faker import Faker
 
 from nise.copy import copy_to_local_dir
@@ -34,7 +37,7 @@ from nise.upload import upload_to_s3
 
 def _write_csv(output_file, data, header=COLUMNS):
     """Output csv file data."""
-    with output_file as file:
+    with open(output_file, 'w') as file:
         writer = csv.DictWriter(file, fieldnames=header)
         writer.writeheader()
         for row in data:
@@ -75,6 +78,41 @@ def route_file(bucket_name, bucket_file_path, local_path):
                      local_path)
 
 
+def _create_month_list(start_date, end_date):
+    """Create a list of months given the date range args."""
+    months = []
+    current = start_date
+
+    if start_date.month == end_date.month:
+        # No month range
+        month = {}
+        month['name'] = calendar.month_name[start_date.month]
+        month['start'] = start_date
+        month['end'] = end_date
+        months.append(month)
+    else:
+        while current <= end_date:
+            month = {}
+            month['name'] = calendar.month_name[current.month]
+            if current.month == start_date.month:
+                # First month
+                month['start'] = start_date
+                month['end'] = datetime(year=current.year,
+                                        month=current.month,
+                                        day=calendar.monthrange(year=current.year,
+                                                                month=current.month)[1])
+            else:
+                month['start'] = datetime(year=current.year, month=current.month, day=1)
+                month['end'] = datetime(year=current.year,
+                                        month=current.month,
+                                        day=calendar.monthrange(year=current.year,
+                                                                month=current.month)[1])
+            months.append(month)
+            current += relativedelta(months=+1)
+
+    return months
+
+
 # pylint: disable=too-many-locals
 def create_report(output_file, options):
     """Create a cost usage report file."""
@@ -82,6 +120,7 @@ def create_report(output_file, options):
     data = []
     start_date = options.get('start_date')
     end_date = options.get('end_date')
+    months = _create_month_list(start_date, end_date)
     fake = Faker()
     payer_account = fake.ean(length=13)  # pylint: disable=no-member
     usage_accounts = (payer_account,
@@ -89,32 +128,43 @@ def create_report(output_file, options):
                       fake.ean(length=13),  # pylint: disable=no-member
                       fake.ean(length=13),  # pylint: disable=no-member
                       fake.ean(length=13))  # pylint: disable=no-member
-    for generator in generators:
-        gen = generator(start_date, end_date, payer_account, usage_accounts)
-        data += gen.generate_data()
+    for month in months:
+        for generator in generators:
+            gen = generator(month.get('start'), month.get('end'), payer_account, usage_accounts)
+            data += gen.generate_data()
 
-    _write_csv(output_file, data)
+        file_path = os.path.dirname(output_file.name)
+        if not file_path:
+            file_path = os.getcwd()
+        file_name = os.path.basename(output_file.name)
+        month_output_file_name = '{}-{}-{}'.format(month.get('name'),
+                                                   month.get('start').year,
+                                                   file_name)
+        month_output_file = '{}/{}'.format(file_path, month_output_file_name)
+        _write_csv(month_output_file, data)
 
-    bucket_name = options.get('bucket_name')
-    if bucket_name:
-        report_name = options.get('report_name')
-        manifest_values = {'account': payer_account}
-        manifest_values.update(options)
-        s3_cur_path, manifest_data = generate_manifest(fake, manifest_values)
-        s3_assembly_path = os.path.dirname(s3_cur_path)
-        s3_month_path = os.path.dirname(s3_assembly_path)
-        s3_month_manifest_path = s3_month_path + '/' + report_name + '-Manifest.json'
-        s3_assembly_manifest_path = s3_assembly_path + '/' + report_name + '-Manifest.json'
-        temp_manifest = _write_manifest(manifest_data)
-        temp_cur_zip = _gzip_report(output_file.name)
-        route_file(bucket_name,
-                   s3_month_manifest_path,
-                   temp_manifest)
-        route_file(bucket_name,
-                   s3_assembly_manifest_path,
-                   temp_manifest)
-        route_file(bucket_name,
-                   s3_cur_path,
-                   temp_cur_zip)
-        os.remove(temp_manifest)
-        os.remove(temp_cur_zip)
+        bucket_name = options.get('bucket_name')
+        if bucket_name:
+            report_name = options.get('report_name')
+            manifest_values = {'account': payer_account}
+            manifest_values.update(options)
+            manifest_values['start_date'] = month.get('start')
+            manifest_values['end_date'] = month.get('end')
+            s3_cur_path, manifest_data = generate_manifest(fake, manifest_values)
+            s3_assembly_path = os.path.dirname(s3_cur_path)
+            s3_month_path = os.path.dirname(s3_assembly_path)
+            s3_month_manifest_path = s3_month_path + '/' + report_name + '-Manifest.json'
+            s3_assembly_manifest_path = s3_assembly_path + '/' + report_name + '-Manifest.json'
+            temp_manifest = _write_manifest(manifest_data)
+            temp_cur_zip = _gzip_report(month_output_file)
+            route_file(bucket_name,
+                       s3_month_manifest_path,
+                       temp_manifest)
+            route_file(bucket_name,
+                       s3_assembly_manifest_path,
+                       temp_manifest)
+            route_file(bucket_name,
+                       s3_cur_path,
+                       temp_cur_zip)
+            os.remove(temp_manifest)
+            os.remove(temp_cur_zip)
--- tests/test_report.py
+++ tests/test_report.py
@@ -25,7 +25,7 @@ from unittest.mock import ANY, patch
 
 from moto import mock_s3
 
-from nise.report import create_report, _write_csv, _write_manifest
+from nise.report import create_report, _create_month_list, _write_csv, _write_manifest
 
 
 class ReportTestCase(TestCase):
@@ -39,7 +39,7 @@ class ReportTestCase(TestCase):
         headers = ['col1', 'col2']
         data = [{'col1': 'r1c1', 'col2': 'r1c2'},
                 {'col1': 'r2c1', 'col2': 'r2c2'}]
-        _write_csv(temp_file, data, headers)
+        _write_csv(temp_file.name, data, headers)
         self.assertTrue(os.path.exists(temp_file.name))
         os.remove(temp_file.name)
 
@@ -54,7 +54,7 @@ class ReportTestCase(TestCase):
     @patch('nise.report._write_csv')
     def test_create_report_no_s3(self, write_csv):
         """Test the report creation method no s3."""
-        temp_file = NamedTemporaryFile()
+        temp_file = NamedTemporaryFile(mode='w')
         now = datetime.datetime.now().replace(microsecond=0, second=0, minute=0)
         one_day = datetime.timedelta(days=1)
         yesterday = now - one_day
@@ -65,7 +65,7 @@ class ReportTestCase(TestCase):
     @patch('nise.report._write_csv')
     def test_create_report_with_s3(self, write_csv):
         """Test the report creation method with s3."""
-        temp_file = NamedTemporaryFile()
+        temp_file = NamedTemporaryFile(mode='w')
         now = datetime.datetime.now().replace(microsecond=0, second=0, minute=0)
         one_day = datetime.timedelta(days=1)
         yesterday = now - one_day
@@ -73,13 +73,29 @@ class ReportTestCase(TestCase):
                    'end_date': now,
                    'bucket_name': 'my_bucket',
                    'report_name': 'cur_report'}
-        create_report(temp_file, options)
+        create_report(temp_file.name, options)
         write_csv.assert_called_once_with(ANY, ANY)
 
     @patch('nise.report._write_csv')
     def test_create_report_with_local_dir(self, write_csv):
         """Test the report creation method with local directory."""
-        temp_file = NamedTemporaryFile()
+        temp_file = NamedTemporaryFile(mode='w')
+        now = datetime.datetime.now().replace(microsecond=0, second=0, minute=0)
+        one_day = datetime.timedelta(days=1)
+        yesterday = now - one_day
+        local_bucket_path = mkdtemp()
+        options = {'start_date': yesterday,
+                   'end_date': now,
+                   'bucket_name': local_bucket_path,
+                   'report_name': 'cur_report'}
+        create_report(temp_file.name, options)
+        write_csv.assert_called_once_with(ANY, ANY)
+        shutil.rmtree(local_bucket_path)
+
+    @patch('nise.report._write_csv')
+    def test_create_report_no_path(self, write_csv):
+        """Test the report creation method with no file path specified."""
+        temp_file = open('test.csv', 'w+')
         now = datetime.datetime.now().replace(microsecond=0, second=0, minute=0)
         one_day = datetime.timedelta(days=1)
         yesterday = now - one_day
@@ -91,3 +107,29 @@ class ReportTestCase(TestCase):
         create_report(temp_file, options)
         write_csv.assert_called_once_with(ANY, ANY)
         shutil.rmtree(local_bucket_path)
+
+    def test_create_month_list(self):
+        """Test to create month lists."""
+        test_matrix = [{
+                        'start_date': datetime.datetime(year=2018, month=1, day=15),
+                        'end_date': datetime.datetime(year=2018, month=1, day=30),
+                        'expected_list': [{'name': 'January',
+                                           'start': datetime.datetime(year=2018, month=1, day=15),
+                                           'end': datetime.datetime(year=2018, month=1, day=30)}]},
+                        {
+                        'start_date': datetime.datetime(year=2018, month=1, day=15),
+                        'end_date': datetime.datetime(year=2018, month=3, day=30),
+                        'expected_list': [{'name': 'January',
+                                           'start': datetime.datetime(year=2018, month=1, day=15),
+                                           'end': datetime.datetime(year=2018, month=1, day=31)},
+                                           {'name': 'February',
+                                           'start': datetime.datetime(year=2018, month=2, day=1),
+                                           'end': datetime.datetime(year=2018, month=2, day=28)},
+                                           {'name': 'March',
+                                           'start': datetime.datetime(year=2018, month=3, day=1),
+                                           'end': datetime.datetime(year=2018, month=3, day=31)}]},                    
+                      ]
+        
+        for test_case in test_matrix:
+            output = _create_month_list(test_case['start_date'], test_case['end_date'])
+            self.assertCountEqual(output, test_case['expected_list'])
