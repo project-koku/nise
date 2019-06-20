7f0889b385a3bc04d78e449e1dd4bdc2a6cae688
Andrew Berglund <aberglun@redhat.com>
2019-06-20 12:47:56 -0400

    Silly LIIINNNNT

--- nise/report.py
+++ nise/report.py
@@ -131,8 +131,8 @@ def ocp_route_file(insights_upload, local_path):
             response = requests.post(insights_upload,
                                      data={},
                                      files={'file': ('payload.tar.gz',
-                                                       upload_file,
-                                                       'application/vnd.redhat.hccm.tar+tgz')},
+                                                     upload_file,
+                                                     'application/vnd.redhat.hccm.tar+tgz')},
                                      auth=(insights_user, insights_password),
                                      verify=False)
             if response.status_code == 202:
