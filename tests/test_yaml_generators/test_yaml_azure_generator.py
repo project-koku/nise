35158cddefc467f52287530ee7f38cf1168e910f
Michael Skarbek <mskarbek@redhat.com>
2020-05-29 17:26:55 -0400

    update keys in test

--- tests/test_yaml_generators/test_yaml_azure_generator.py
+++ tests/test_yaml_generators/test_yaml_azure_generator.py
@@ -83,7 +83,18 @@ class AzureGeneratorTestCase(TestCase):
             return v_min <= val <= config_val
 
         def validate_data(data, config, check_func):
-            keys = sorted(["start_date", "end_date", "meter_id", "resource_location", "tags"])
+            keys = sorted(
+                [
+                    "start_date",
+                    "end_date",
+                    "instance_id",
+                    "meter_id",
+                    "resource_location",
+                    "usage_quantity",
+                    "resource_rate",
+                    "pre_tax_cost",
+                ]
+            )
             gens = ["bandwidth_gens", "sql_gens", "storage_gens", "vmachine_gens", "vnetwork_gens"]
 
             self.assertTrue(isinstance(data, self.module.dicta))
