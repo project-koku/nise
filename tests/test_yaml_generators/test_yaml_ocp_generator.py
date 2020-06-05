c309359ad87ed11003ba3969e4cf08047a966f87
Michael Skarbek <mskarbek@redhat.com>
2020-06-05 14:09:13 -0400

    require at least 1 of each in ocp

--- tests/test_yaml_generators/test_yaml_ocp_generator.py
+++ tests/test_yaml_generators/test_yaml_ocp_generator.py
@@ -81,7 +81,7 @@ class OCPGeneratorTestCase(TestCase):
         def check_exact(val, config_val, **kwargs):
             return val == config_val
 
-        def check_range(val, config_val, v_min=0):
+        def check_range(val, config_val, v_min=1):
             return v_min <= val <= config_val
 
         def validate_data(data, config, check_func):
