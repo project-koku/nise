cf47e2771ffbf97b77088993ea74354ee3c38442
Michael Skarbek <mskarbek@redhat.com>
2020-06-05 09:53:57 -0400

    [COST-94] Add Aws users to yaml generator (#249)
    * aws yaml generator adds users to yaml
    
    * bump version
    
    * add regions. lay groundwork to allow custom tags from cli

--- nise/generators/aws/aws_generator.py
+++ nise/generators/aws/aws_generator.py
@@ -163,30 +163,72 @@ RESERVE_COLS = (
     "reservation/TotalReservedUnits",
     "reservation/UnitsPerReservation",
 )
-RESOURCE_TAG_COLS = (
-    "resourceTags/user:environment",
-    "resourceTags/user:app",
-    "resourceTags/user:version",
-    "resourceTags/user:storageclass",
-    "resourceTags/user:openshift_cluster",
-    "resourceTags/user:openshift_project",
-    "resourceTags/user:openshift_node",
-)
 
 
 class AWSGenerator(AbstractGenerator):
     """Defines a abstract class for generators."""
 
+    RESOURCE_TAG_COLS = {
+        "resourceTags/user:environment",
+        "resourceTags/user:app",
+        "resourceTags/user:version",
+        "resourceTags/user:storageclass",
+        "resourceTags/user:openshift_cluster",
+        "resourceTags/user:openshift_project",
+        "resourceTags/user:openshift_node",
+    }
     AWS_COLUMNS = set(
-        IDENTITY_COLS + BILL_COLS + LINE_ITEM_COLS + PRODUCT_COLS + PRICING_COLS + RESERVE_COLS + RESOURCE_TAG_COLS
+        IDENTITY_COLS
+        + BILL_COLS
+        + LINE_ITEM_COLS
+        + PRODUCT_COLS
+        + PRICING_COLS
+        + RESERVE_COLS
+        + tuple(RESOURCE_TAG_COLS)
     )
     REGIONS = (
         ("US East (N. Virginia)", "us-east-1", "us-east-1a", "USE1-EBS"),
         ("US East (N. Virginia)", "us-east-1", "us-east-1b", "USE1-EBS"),
+        ("US East (N. Virginia)", "us-east-1", "us-east-1c", "USE1-EBS"),
+        ("US East (N. Virginia)", "us-east-1", "us-east-1d", "USE1-EBS"),
+        ("US East (N. Virginia)", "us-east-1", "us-east-1e", "USE1-EBS"),
+        ("US East (N. Virginia)", "us-east-1", "us-east-1f", "USE1-EBS"),
         ("US West (N. California)", "us-west-1", "us-west-1a", "USW1-EBS"),
         ("US West (N. California)", "us-west-1", "us-west-1b", "USW1-EBS"),
         ("US West (Oregon)", "us-west-2", "us-west-2a", "USW2-EBS"),
         ("US West (Oregon)", "us-west-2", "us-west-2b", "USW2-EBS"),
+        ("US West (Oregon)", "us-west-2", "us-west-2c", "USW2-EBS"),
+        ("Europe (Stockholm)", "eu-north-1", "eu-north-1a", "EUN1-EBS"),
+        ("Europe (Ireland)", "eu-west-1", "eu-west-1a", "EUW1-EBS"),
+        ("Europe (Ireland)", "eu-west-1", "eu-west-1b", "EUW1-EBS"),
+        ("Europe (Ireland)", "eu-west-1", "eu-west-1c", "EUW1-EBS"),
+        ("Europe (Frankfurt)", "eu-central-1", "eu-central-1a", "EUC1-EBS"),
+        ("Europe (Frankfurt)", "eu-central-1", "eu-central-1b", "EUC1-EBS"),
+        ("Europe (London)", "eu-west-2", "eu-west-2a", "EUW2_EBS"),
+        ("Europe (Paris)", "eu-west-3", "eu-west-3a", "EUW3_EBS"),
+        ("Europe (Milan)", "eu-south-1", "eu-south-1", "EUN1-EBS"),
+        ("Middle East (Bahrain)", "me-south-1", "me-south-1a", "MES1-EBS"),
+        ("Asia Pacific (Hong Kong)", "ap-east-1", "ap-east-1a", "APE1-EBS"),
+        ("Asia Pacific (Singapore)", "ap-southeast-1", "ap-southeast-1a", "APSE1-EBS"),
+        ("Asia Pacific (Singapore)", "ap-southeast-1", "ap-southeast-1b", "APSE1-EBS"),
+        ("Asia Pacific (Sydney)", "ap-southeast-2", "ap-southeast-2a", "APSE2-EBS"),
+        ("Asia Pacific (Sydney)", "ap-southeast-2", "ap-southeast-2b", "APSE2-EBS"),
+        ("Asia Pacific (Sydney)", "ap-southeast-2", "ap-southeast-2c", "APSE2-EBS"),
+        ("Asia Pacific (Tokyo)", "ap-northeast-1", "ap-northeast-1a", "APNE1-EBS"),
+        ("Asia Pacific (Tokyo)", "ap-northeast-1", "ap-northeast-1c", "APNE1-EBS"),
+        ("Asia Pacific (Seoul)", "ap-northeast-2", "ap-northeast-2a", "APNE2-EBS"),
+        ("Asia Pacific (Osaka-Local)", "ap-northeast-3", "ap-northeast-3a", "APNE3-EBS"),
+        ("China (Beijing)", "cn-north-1", "cn-north-1a", "CNN1-EBS"),
+        ("China (Ningxia)", "cn-northwest-1", "cn-northwest-1a", "CNNW1-EBS"),
+        ("SA East (Sao Paulo)", "sa-east-1", "sa-east-1a", "SA1-EBS"),
+        ("SA East (Sao Paulo)", "sa-east-1", "sa-east-1b", "SA1-EBS"),
+        ("SA East (Sao Paulo)", "sa-east-1", "sa-east-1c", "SA1-EBS"),
+        ("Asia Pacific (Mumbai)	", "ap-south-1", "ap-south-1a", "APS1-EBS"),
+        ("Asia Pacific (Mumbai)	", "ap-south-1", "ap-south-1b", "APS1-EBS"),
+        ("Canada (Central)", "ca-central-1", "ca-central-1a", "CAC1-EBS"),
+        ("Africa (Cape Town)", "af-south-1", "af-south-1a", "AFS1-EBS"),
+        ("AWS GovCloud (US-East)", "us-gov-east-1", "us-gov-east-1a", "USGE1-EBS"),
+        ("AWS GovCloud (US)", "us-gov-west-1", "us-gov-west-1a", "USGW1-EBS"),
     )
 
     def __init__(self, start_date, end_date, payer_account, usage_accounts, attributes=None, tag_cols=None):
@@ -197,6 +239,7 @@ class AWSGenerator(AbstractGenerator):
         self._tags = None
         self.num_instances = 1 if attributes else randint(2, 60)
         if tag_cols:
+            self.RESOURCE_TAG_COLS.update(tag_cols)
             self.AWS_COLUMNS.update(tag_cols)
         super().__init__(start_date, end_date)
 
@@ -256,7 +299,7 @@ class AWSGenerator(AbstractGenerator):
         """Pick instance location."""
         if self.attributes and self.attributes.get("region"):
             region = self.attributes.get("region")
-            location = [option for option in self.REGIONS if region in option].pop()
+            location = choice([option for option in self.REGIONS if region in option])
         else:
             location = choice(self.REGIONS)
         return location
@@ -275,10 +318,13 @@ class AWSGenerator(AbstractGenerator):
             for tag in self._tags:
                 row[tag] = self._tags[tag]
         else:
-            row["resourceTags/user:environment"] = self._pick_tag(
-                "resourceTags/user:environment", ("dev", "ci", "qa", "stage", "prod")
-            )
-            row["resourceTags/user:version"] = self._pick_tag("resourceTags/user:version", ("alpha", "beta"))
+            num_tags = self.fake.random_int(0, 5)
+            for _ in range(num_tags):
+                seen_tags = set()
+                tag_key = choice(list(self.RESOURCE_TAG_COLS))
+                if tag_key not in seen_tags:
+                    row[tag_key] = self.fake.word()
+                    seen_tags.update([tag_key])
 
     def _generate_region_short_code(self, region):
         """Generate the AWS short code for a region."""
--- nise/yaml_generators/aws/generator.py
+++ nise/yaml_generators/aws/generator.py
@@ -30,6 +30,7 @@ from nise.yaml_generators.aws.rds_instance_types import INSTANCE_TYPES as RDS_IN
 from nise.yaml_generators.aws.regions import REGIONS
 from nise.yaml_generators.generator import Generator
 from nise.yaml_generators.utils import dicta
+from nise.yaml_generators.utils import generate_account_id
 from nise.yaml_generators.utils import generate_name
 
 LOG = logging.getLogger(__name__)
@@ -108,6 +109,7 @@ class AWSGenerator(Generator):
             route53_gens=[],
             s3_gens=[],
             vpc_gens=[],
+            users=[],
         )
 
         max_data_transfer_gens = (
@@ -119,6 +121,7 @@ class AWSGenerator(Generator):
         max_route53_gens = FAKER.random_int(1, config.max_route53_gens) if _random else config.max_route53_gens
         max_s3_gens = FAKER.random_int(1, config.max_s3_gens) if _random else config.max_s3_gens
         max_vpc_gens = FAKER.random_int(1, config.max_vpc_gens) if _random else config.max_vpc_gens
+        max_users = FAKER.random_int(1, config.max_users) if _random else config.max_users
 
         LOG.info(f"Building {max_data_transfer_gens} data transfer generators ...")
         for _ in range(max_data_transfer_gens):
@@ -174,6 +177,10 @@ class AWSGenerator(Generator):
             vpc_gen = initialize_dicta("VPC", config)
             data.vpc_gens.append(vpc_gen)
 
+        LOG.info(f"Adding {max_users} users.")
+        for _ in range(max_users):
+            data.users.append(generate_account_id(config))
+
         return data
 
     def default_config(self):
@@ -189,6 +196,8 @@ class AWSGenerator(Generator):
             start_date=default_date.replace(day=1) - relativedelta(months=1),
             end_date=default_date.replace(day=last_day_of_month),
             payer_account=9999999999999,
+            max_account_id_length=13,
+            max_users=1,
             max_name_words=2,
             max_resource_id_length=10,
             max_data_transfer_gens=1,
@@ -214,6 +223,8 @@ class AWSGenerator(Generator):
             start_date=date,
             end_date=date,
             payer_account=int,
+            max_account_id_length=int,
+            max_users=int,
             max_name_words=int,
             max_resource_id_length=int,
             max_data_transfer_gens=int,
