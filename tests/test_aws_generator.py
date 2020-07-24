810f189c79ad25f7a9413de7e3f87654f34a8066
Brett Lentz <blentz@redhat.com>
2020-07-24 15:13:37 -0400

    use subtest instead of a random generator

--- tests/test_aws_generator.py
+++ tests/test_aws_generator.py
@@ -18,7 +18,6 @@ import os
 import tempfile
 from datetime import datetime
 from datetime import timedelta
-from random import choice
 from unittest import TestCase
 
 from faker import Faker
@@ -92,9 +91,8 @@ class AbstractGeneratorTestCase(TestCase):
             self.fake.ean(length=13),
             self.fake.ean(length=13),
         )
-        self.TestGenerator = choice(AWS_GENERATORS)
         self.test_config_kwargs = {
-            "generator": self.TestGenerator.__name__,
+            "generator": None,
             "start_date": self.now - (2 * self.one_hour),
             "end_date": self.now,
             "resource_id": self.fake.word(),
@@ -109,99 +107,128 @@ class AbstractGeneratorTestCase(TestCase):
 
     def test_set_hours_invalid_start(self):
         """Test that the start date must be a date object."""
-        with self.assertRaises(ValueError):
-            self.TestGenerator("invalid", self.now)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                with self.assertRaises(ValueError):
+                    TestGenerator("invalid", self.now)
 
     def test_set_hours_invalid_end(self):
         """Test that the end date must be a date object."""
-        with self.assertRaises(ValueError):
-            self.TestGenerator(self.now, "invalid")
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                with self.assertRaises(ValueError):
+                    TestGenerator(self.now, "invalid")
 
     def test_set_hours_none_start(self):
         """Test that the start date is not None."""
-        with self.assertRaises(ValueError):
-            self.TestGenerator(None, self.now)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                with self.assertRaises(ValueError):
+                    TestGenerator(None, self.now)
 
     def test_set_hours_none_end(self):
         """Test that the end date is not None."""
-        with self.assertRaises(ValueError):
-            self.TestGenerator(self.now, None)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                with self.assertRaises(ValueError):
+                    TestGenerator(self.now, None)
 
     def test_set_hours_compared_dates(self):
         """Test that the start date must be less than the end date."""
         hour_ago = self.now - self.one_hour
-        with self.assertRaises(ValueError):
-            self.TestGenerator(self.now, hour_ago)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                with self.assertRaises(ValueError):
+                    TestGenerator(self.now, hour_ago)
 
     def test_set_hours(self):
         """Test that a valid list of hours are returned."""
         two_hours_ago = (self.now - self.one_hour) - self.one_hour
-        generator = self.TestGenerator(two_hours_ago, self.now)
-        expected = [
-            {"start": two_hours_ago, "end": two_hours_ago + self.one_hour},
-            {"start": two_hours_ago + self.one_hour, "end": two_hours_ago + self.one_hour + self.one_hour},
-        ]
-        self.assertEqual(generator.hours, expected)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                generator = TestGenerator(two_hours_ago, self.now)
+                expected = [
+                    {"start": two_hours_ago, "end": two_hours_ago + self.one_hour},
+                    {"start": two_hours_ago + self.one_hour, "end": two_hours_ago + self.one_hour + self.one_hour},
+                ]
+                self.assertEqual(generator.hours, expected)
 
     def test_timestamp_none(self):
         """Test that the timestamp method fails with None."""
-        with self.assertRaises(ValueError):
-            self.TestGenerator.timestamp(None)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                with self.assertRaises(ValueError):
+                    TestGenerator.timestamp(None)
 
     def test_timestamp_invalid(self):
         """Test that the timestamp method fails with an not a date."""
-        with self.assertRaises(ValueError):
-            self.TestGenerator.timestamp("invalid")
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                with self.assertRaises(ValueError):
+                    TestGenerator.timestamp("invalid")
 
     def test_init_data_row(self):
         """Test the init data row method."""
         two_hours_ago = (self.now - self.one_hour) - self.one_hour
-        generator = self.TestGenerator(two_hours_ago, self.now)
-        a_row = generator._init_data_row(two_hours_ago, self.now)
-        self.assertIsInstance(a_row, dict)
-        for col in generator.AWS_COLUMNS:
-            self.assertIsNotNone(a_row.get(col))
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                generator = TestGenerator(two_hours_ago, self.now)
+                a_row = generator._init_data_row(two_hours_ago, self.now)
+                self.assertIsInstance(a_row, dict)
+                for col in generator.AWS_COLUMNS:
+                    self.assertIsNotNone(a_row.get(col))
 
     def test_init_data_row_start_none(self):
         """Test the init data row method none start date."""
         two_hours_ago = (self.now - self.one_hour) - self.one_hour
-        generator = self.TestGenerator(two_hours_ago, self.now)
-        with self.assertRaises(ValueError):
-            generator._init_data_row(None, self.now)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                generator = TestGenerator(two_hours_ago, self.now)
+                with self.assertRaises(ValueError):
+                    generator._init_data_row(None, self.now)
 
     def test_init_data_row_end_none(self):
         """Test the init data row method none end date."""
         two_hours_ago = (self.now - self.one_hour) - self.one_hour
-        generator = self.TestGenerator(two_hours_ago, self.now)
-        with self.assertRaises(ValueError):
-            generator._init_data_row(two_hours_ago, None)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                generator = TestGenerator(two_hours_ago, self.now)
+                with self.assertRaises(ValueError):
+                    generator._init_data_row(two_hours_ago, None)
 
     def test_init_data_row_start_invalid(self):
         """Test the init data row method invalid start date."""
         two_hours_ago = (self.now - self.one_hour) - self.one_hour
-        generator = self.TestGenerator(two_hours_ago, self.now)
-        with self.assertRaises(ValueError):
-            generator._init_data_row("invalid", self.now)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                generator = TestGenerator(two_hours_ago, self.now)
+                with self.assertRaises(ValueError):
+                    generator._init_data_row("invalid", self.now)
 
     def test_init_data_row_end_invalid(self):
         """Test the init data row method invalid end date."""
         two_hours_ago = (self.now - self.one_hour) - self.one_hour
-        generator = self.TestGenerator(two_hours_ago, self.now)
-        with self.assertRaises(ValueError):
-            generator._init_data_row(two_hours_ago, "invalid")
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                generator = TestGenerator(two_hours_ago, self.now)
+                with self.assertRaises(ValueError):
+                    generator._init_data_row(two_hours_ago, "invalid")
 
     def test_get_location(self):
         """Test the _get_location method."""
         two_hours_ago = (self.now - self.one_hour) - self.one_hour
-        generator = self.TestGenerator(two_hours_ago, self.now)
-        location = generator._get_location()
-
-        self.assertIsInstance(location, tuple)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                generator = TestGenerator(two_hours_ago, self.now)
+                location = generator._get_location()
+                self.assertIsInstance(location, tuple)
 
         region = "us-west-1"
-        generator = self.TestGenerator(two_hours_ago, self.now)
-        location = generator._get_location(config={"region": region})
-        self.assertIn(region, location)
+        for TestGenerator in AWS_GENERATORS:
+            with self.subTest(generator=TestGenerator.__name__):
+                generator = TestGenerator(two_hours_ago, self.now)
+                location = generator._get_location(config={"region": region})
+                self.assertIn(region, location)
 
 
 class AWSGeneratorTestCase(TestCase):
