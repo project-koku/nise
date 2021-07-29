cc352464686c0be5a8f4d7fd62628caa3387f50d
Doug Curtis <docurtis@redhat.com>
2021-07-29 14:47:23 -0400

    more fixes

--- nise/report.py
+++ nise/report.py
@@ -285,6 +285,8 @@ def _create_month_list(start_date, end_date):
         if current.month == start_date.month:
             # First month start with start_date
             month["start"] = start_date
+        if current.month < end_date.month:
+            month["end"] = (month.get("end") + relativedelta(days=1)).replace(hour=0, minute=0)
         if current.month == end_date.month:
             # Last month ends with end_date
             month["end"] = end_date.replace(hour=23, minute=59)
