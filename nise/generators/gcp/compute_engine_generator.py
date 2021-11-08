4c8bc2d44a879a867c350b2fe37aef881f619df7
Luke Couzens <lcouzens@redhat.com>
2021-11-08 18:18:36 +0000

    Adding imporved credit amount distribution for gcp (#375)

--- nise/generators/gcp/compute_engine_generator.py
+++ nise/generators/gcp/compute_engine_generator.py
@@ -89,7 +89,7 @@ class ComputeEngineGenerator(GCPGenerator):
         row["usage.amount_in_pricing_units"] = self._gen_pricing_unit_amount(pricing_unit, row["usage.amount"])
         cost = self._gen_cost(row["usage.amount_in_pricing_units"])
         row["cost"] = cost
-        credit, credit_total = self._gen_credit(cost, self.credit_total, self._credit_amount)
+        credit, credit_total = self._gen_credit(self.credit_total, self._credit_amount)
         self.credit_total = credit_total
         row["credits"] = credit
         usage_date = datetime.strptime(row.get("usage_start_time"), "%Y-%m-%dT%H:%M:%S")
@@ -143,7 +143,7 @@ class JSONLComputeEngineGenerator(ComputeEngineGenerator):
         cost = self._gen_cost(usage["amount_in_pricing_units"])
         row["cost"] = cost
         row["usage"] = usage
-        credit, credit_total = self._gen_credit(cost, self.credit_total, self._credit_amount, True)
+        credit, credit_total = self._gen_credit(self.credit_total, self._credit_amount, True)
         self.credit_total = credit_total
         row["credits"] = credit
         row["cost_type"] = "regular"
