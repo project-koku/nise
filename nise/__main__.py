8dc966ad6bec6f50eb6c73da9066ffcf9822951f
David N <dnakabaa@redhat.com>
2022-04-22 08:07:34 -0400

    add oci to list of providers when validating provider inputs

--- nise/__main__.py
+++ nise/__main__.py
@@ -572,7 +572,7 @@ def _validate_provider_inputs(parser, options):
         valid_inputs = func(parser, options)
     else:
         msg = "One of {}, {}, {}, or {} must be supplied to generate a report."
-        msg = msg.format("aws", "aws-marketplace", "azure", "ocp", "gcp")
+        msg = msg.format("aws", "aws-marketplace", "azure", "ocp", "gcp", "oci")
         parser.error(msg)
     return (valid_inputs, provider_type)
 
