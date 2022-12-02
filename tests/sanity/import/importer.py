cff5fd3e0d05a1363e752423adc745d0a8db9e43
Sam Doran <sdoran@redhat.com>
2022-12-02 14:47:36 -0500

    Add import sanity tests (#423)

---
+++ tests/sanity/import/importer.py
@@ -0,0 +1,53 @@
+def main():  # noqa: C901
+    """Isolate globals from imported code"""
+
+    import os
+    import pathlib
+    import sys
+    import traceback
+
+    from importlib import import_module
+
+    def convert_path_to_name(path: str) -> str:
+        clean_path = pathlib.Path(path)
+        if clean_path.name == "__init__.py":
+            clean_path = clean_path.parent
+
+        clean_path = clean_path.with_suffix("")
+        name = str(clean_path).replace(os.path.sep, ".")
+
+        return name
+
+    def test_module(path: str, name: str, messages: set) -> None:
+        try:
+            import_module(name)
+        except Exception as exc:
+            exc_type, _exc, exc_tb = sys.exc_info()
+            frames = reversed(traceback.extract_tb(exc_tb))
+            line = 0
+            message = str(exc)
+            for frame in frames:
+                if frame.filename.endswith(path):
+                    line = frame.lineno or 0
+                    break
+
+            if message not in messages:
+                print(f"{path}:{line}: traceback: {exc_type.__name__} {message}")
+                messages.add(message)
+
+    messages = set()
+    files = sys.argv[1:]
+    if not files:
+        source = pathlib.Path(__file__).parent.parent.parent.parent.joinpath("nise")
+        files = [str(file) for file in source.rglob("*.py")]
+
+    for path in files:
+        name = convert_path_to_name(path)
+        test_module(path, name, messages)
+
+    if messages:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
