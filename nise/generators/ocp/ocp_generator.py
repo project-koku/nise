4663ed12bf4e7e765a0ac0eec8f02a470f3f83df
Eva Sebestova <esebesto@redhat.com>
2023-04-20 12:17:07 +0200

    update calc of randomized usage

--- nise/generators/ocp/ocp_generator.py
+++ nise/generators/ocp/ocp_generator.py
@@ -160,15 +160,15 @@ def get_owner_workload(pod):
 def generate_randomized_ros_usage(usage_dict, limit_value):
     # if usage value is provided in yaml -> avg_value = +- 5% of that specified usage value
     if usage_value := usage_dict.get("full_period"):
-        avg_value = min(round(uniform(usage_value * 0.95, usage_value * 1.05), 5), limit_value * 0.95)
-    # if usage value is not specified in yaml -> random avg_usage from 10% to 95% of the limit
+        avg_value = min(round(uniform(usage_value * 0.95, usage_value * 1.05), 5), limit_value)
+    # if usage value is not specified in yaml -> random avg_usage from 10% to 100% of the limit
     else:
-        avg_value = round(uniform(limit_value * 0.1, limit_value * 0.95), 5)
+        avg_value = round(uniform(limit_value * 0.1, limit_value), 5)
 
     # min value - random float derived from avg_value,
-    min_value = round(uniform(avg_value * 0.9, avg_value * 0.98), 5)
+    min_value = round(uniform(avg_value * 0.8, avg_value), 5)
     # max_value - random float derived from avg_value, but max of limit_value
-    max_value = min(round(uniform(avg_value * 1.02, avg_value * 1.1), 5), limit_value)
+    max_value = min(round(uniform(avg_value, avg_value * 1.2), 5), limit_value)
 
     return avg_value, min_value, max_value
 
