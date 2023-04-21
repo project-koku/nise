16c2e3e212430f6c4d47eccf1047e38637d89603
Sam Doran <sdoran@redhat.com>
2023-04-21 15:44:28 -0400

    Modernize installation
    - Create pyproject.toml
    - Move configuration out of setup.py and into setup.cfg
    - Improve version parsing
    - Move requirements to a separate file

--- setup.py
+++ setup.py
@@ -14,54 +14,15 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-import codecs
-import os.path
+from pathlib import Path
 
 from setuptools import find_packages
 from setuptools import setup
 
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), "r") as fp:
-        return fp.read()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
+here = Path(__file__).parent.resolve()
+install_requires = here.joinpath("requirements.txt").read_text().splitlines()
 
 setup(
-    name="koku-nise",
-    version=get_version("nise/__init__.py"),  # set the version number in nise.__init__
-    author="Project Koku",
-    author_email="cost_mgmt@redhat.com",
-    description="A tool for generating sample cost and usage data for testing purposes.",
-    url="https://github.com/project-koku/nise",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: OS Independent",
-    ],
     packages=find_packages(),
-    install_requires=[
-        "faker>=3.0",
-        "boto3>=1.11",
-        "requests>=2.22",
-        "jinja2>=2.10",
-        "azure-storage-blob>=12.1",
-        "google-cloud-storage>=1.19",
-        "pyyaml>=5.3",
-        "google-cloud-bigquery>=2.2.0",
-        "oci>=2.64.0",
-    ],
-    dependency_links=[],
-    entry_points={"console_scripts": ["nise = nise.__main__:main"]},
-    include_package_data=True,
-    zip_safe=False,
+    install_requires=install_requires,
 )
