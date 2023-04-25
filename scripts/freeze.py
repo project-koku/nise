b43dd2e35d17549b5c07244a7415d65af0efd207
Sam Doran <sdoran@redhat.com>
2023-04-25 16:46:00 -0400

    Use a temp dir for freezing

--- scripts/freeze.py
+++ scripts/freeze.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 import pathlib
 import subprocess
+import tempfile
 import venv
 
 
 def main():
-    freezer_venv = pathlib.Path(".venvs/freezer")
-    venv_python = freezer_venv / "bin" / "python"
-    pip = [venv_python, "-m", "pip", "--disable-pip-version-check"]
     requirements = pathlib.Path("tests/requirements.in")
 
-    venv.create(freezer_venv, clear=True, with_pip=True)
-    subprocess.run([*pip, "install", "-r", requirements], env={})
-    out = subprocess.check_output([*pip, "freeze"], env={}, text=True)
-    requirements.with_suffix(".txt").write_text(out)
+    with tempfile.TemporaryDirectory() as venv_tmpdir:
+        freezer_venv = pathlib.Path(venv_tmpdir)
+        venv_python = freezer_venv / "bin" / "python"
+        pip = [venv_python, "-m", "pip", "--disable-pip-version-check"]
+
+        venv.create(freezer_venv, clear=True, with_pip=True)
+        subprocess.run([*pip, "install", "-r", requirements], env={})
+        out = subprocess.check_output([*pip, "freeze"], env={}, text=True)
+        requirements.with_suffix(".txt").write_text(out)
 
 
 if __name__ == "__main__":
