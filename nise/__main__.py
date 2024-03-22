ab7063fbd669454b0bdb1e3cecf39eb36e4b2362
Eva Sebestova <esebesto@redhat.com>
2024-03-22 10:05:21 +0100

    Fix date issue with attributes

--- nise/__main__.py
+++ nise/__main__.py
@@ -701,7 +701,8 @@ def _load_static_report_data(options):
             end_date = attributes.get("end_date", options.get("end_date"))
             generated_end_date = today()
             if end_date and (
-                end_date.date() != today().date() or (isinstance(end_date, datetime.datetime) and end_date.hour != 0)
+                end_date != today().date() or
+                (isinstance(end_date, datetime.datetime) and (end_date.date() != today().date() or end_date.hour != 0))
             ):
                 generated_end_date = calculate_end_date(generated_start_date, end_date)
 
