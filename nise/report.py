986c86378f93eefb2fe495ab7f2643c4af82da73
Eva Sebestova <esebesto@redhat.com>
2025-09-02 12:23:21 +0200

    final fix

--- nise/report.py
+++ nise/report.py
@@ -1184,7 +1184,7 @@ def gcp_create_report(options):  # noqa: C901
                             # Skip if generator usage is outside of current month
                             if attributes.get("end_date") < month.get("start"):
                                 continue
-                            if attributes.get("start_date") > month.get("end"):
+                            if attributes.get("start_date") > month.get("end").replace(hour=23):
                                 continue
                             gen_start_date, gen_end_date = _create_generator_dates_from_yaml(attributes, month)
                     else:
